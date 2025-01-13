4f4482c6462bcf68635d5a639078706e2ff87a89
Subham <patelsk7481@gmail.com>
2025-01-13 10:56:40 +0530

    Initial Commit

---
+++ EatXtra/EatXtraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatXtraApp.swift
+//  EatXtra
+//
+//  Created by Subham Patel on 13/01/25.
+//
+
+import SwiftUI
+
+@main
+struct EatXtraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EatXtraTests/EatXtraTests.swift
@@ -0,0 +1,17 @@
+//
+//  EatXtraTests.swift
+//  EatXtraTests
+//
+//  Created by Subham Patel on 13/01/25.
+//
+
+import Testing
+@testable import EatXtra
+
+struct EatXtraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
